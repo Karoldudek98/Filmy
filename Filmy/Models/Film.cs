6ec4038eaf4644d465e0642956fc8d2e57c552eb
Karol Dudek <karol.dudek@microsoft.wsei.edu.pl>
2023-11-12 14:38:27 +0100

    new model added

--- Filmy/Models/Film.cs
+++ Filmy/Models/Film.cs
@@ -1,10 +1,17 @@
-﻿namespace Filmy.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Filmy.Models
 {
     public class Film
     {
+        [Key]
         public int Id { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
         public string Tytul { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
         public string Opis { get; set; }
+        [Column(TypeName = "int")]
         public int Ocena { get; set; }
     }
 }
